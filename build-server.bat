fde41312173f5b41045b0af94607439952a3a07e
Max Derbenwick <M.A.Derbenwick@student.tudelft.nl>
2023-04-28 15:41:25 +0200

    Initial commit

---
+++ build-server.bat
@@ -0,0 +1,12 @@
+@echo off
+
+:: Compress beanstalk-server into zip for packaging
+cd beanstalk-server
+tar.exe -a -c -f ../beanstalk-server.zip^
+    app.js^
+    package.json^
+    package-lock.json
+cd ..
+
+:: Automatically deploy to AWS
+:: TODO
