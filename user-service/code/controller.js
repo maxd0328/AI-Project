d4b7216e39c2b01ff7c9db7a8b51f213e170903a
Max Derbenwick <M.A.Derbenwick@student.tudelft.nl>
2023-05-06 19:54:15 +0200

    Forgot/reset password complete

--- user-service/code/controller.js
+++ user-service/code/controller.js
@@ -1,4 +1,13 @@
 const db = require('./database');
+const { v4: uuidv4 } = require('uuid');
+const middleware = require('../commons/middleware');
+const aws = require('../commons/aws');
+
+function enterSession(session, user) {
+    session.loggedIn = true;
+    session.email = user.email;
+    session.firstName = user.firstName;
+}
 
 async function addUser(email, firstName, lastName, phoneNumber, password) {
     const query = `INSERT INTO users (email, firstName, lastName, phoneNumber, password) VALUES (?, ?, ?, ?, ?)`;
@@ -17,7 +26,59 @@ async function authenticateUser(email, password) {
     else return rows[0];
 }
 
+async function userExists(email) {
+    const query = 'SELECT * FROM users WHERE email = ?';
+    const values = [email];
+
+    const [rows] = await db.query(query, values);
+    return rows.length === 1;
+}
+
+async function sendResetPasswordToken(email) {
+    const uuid = uuidv4();
+
+    const tokenKey = `passwordResetToken:${uuid}`;
+    const token = email;
+    const expiry = 60 * 60; // 1 hour
+
+    middleware.redisClient.set(tokenKey, token, 'EX', expiry);
+
+    const message = 'Greetings,\n\nFollow this link to reset your password:\n\n';
+    const link = `http://ai-project-web.us-east-1.elasticbeanstalk.com/user/reset-password?token=${uuid}`;
+    const closure = '\n\nKind regards,\nThe GrAI Matter Team';
+    await aws.sendEmail([email], 'Reset Password', message + link + closure);
+}
+
+async function validateResetPasswordToken(token, remove) {
+    const tokenKey = `passwordResetToken:${token}`;
+    const email = await middleware.redisClient.get(tokenKey);
+
+    if(email) {
+        if(remove)
+            await middleware.redisClient.del(tokenKey);
+
+        return email;
+    }
+    else return null;
+}
+
+async function updateUserPassword(email, password) {
+    let query = `UPDATE users SET password = ? WHERE email = ?`;
+    let values = [password, email];
+    await db.query(query, values);
+
+    query = `SELECT * FROM users WHERE email = ?`;
+    values = [email];
+    const [rows] = await db.query(query, values);
+    return rows[0];
+}
+
 module.exports = {
+    enterSession,
     addUser,
-    authenticateUser
+    authenticateUser,
+    userExists,
+    sendResetPasswordToken,
+    validateResetPasswordToken,
+    updateUserPassword
 };
--- user-service/code/routes.js
+++ user-service/code/routes.js
@@ -1,6 +1,6 @@
-let express = require('express');
-let controller = require('./controller');
-let router = express.Router();
+const express = require('express');
+const controller = require('./controller');
+const router = express.Router();
 
 /* POST create new user. */
 router.post('/create', async function(req, res, next) {
@@ -12,9 +12,7 @@ router.post('/create', async function(req, res, next) {
 
     try {
         await controller.addUser(email, firstName, lastName, phoneNumber, password);
-        req.session.loggedIn = true;
-        req.session.email = email;
-        req.session.firstName = firstName;
+        controller.enterSession(req.session, { email: email, firstName: firstName, lastName: lastName, phoneNumber: phoneNumber, password: password });
         res.redirect('/home');
     }
     catch(err) {
@@ -30,9 +28,7 @@ router.post('/login', async function(req, res, next) {
 
     try {
         const userRow = await controller.authenticateUser(email, password);
-        req.session.loggedIn = true;
-        req.session.email = userRow.email;
-        req.session.firstName = userRow.firstName;
+        controller.enterSession(req.session, userRow);
         res.redirect('/home');
     }
     catch(err) {
@@ -47,6 +43,62 @@ router.get('/logout', async function(req, res, next) {
         if(!err)
             res.redirect('/');
     })
+});
+
+/* POST forgot password request. */
+router.post('/forgot-password', async function(req, res, next) {
+    const email = req.body['email'];
+
+    try {
+        const userExists = controller.userExists(email);
+        if(userExists) {
+            await controller.sendResetPasswordToken(email);
+            res.redirect(`/forgot-password?submitted=true&email=${email}`);
+        }
+        else {
+            // TODO
+            res.redirect('/forgot-password');
+        }
+    }
+    catch(err) {
+        // TODO
+        res.redirect('/forgot-password');
+    }
+});
+
+/* GET reset password page. */
+router.get('/reset-password', async function(req, res, next) {
+    try {
+        const email = await controller.validateResetPasswordToken(req.query.token, false);
+        if(email) res.render('reset-password', { email: email, token: req.query.token });
+        else res.render('invalid-token');
+    }
+    catch(err) {
+        res.render('invalid-token');
+    }
+});
+
+/* POST reset password request. */
+router.post('/reset-password', async function(req, res, next) {
+    const password = req.body['password'];
+    const token = req.body['token'];
+
+    try {
+        const email = await controller.validateResetPasswordToken(token, true);
+        if(email) {
+            const user = await controller.updateUserPassword(email, password);
+            controller.enterSession(req.session, user);
+            res.redirect('/home');
+        }
+        else {
+            // TODO
+            res.redirect(`/user/reset-password?token=${token}`);
+        }
+    }
+    catch(err) {
+        // TODO
+        res.redirect(`/user/reset-password?token=${token}`);
+    }
 });
 
 module.exports = router;
