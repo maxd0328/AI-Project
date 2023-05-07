548b925695b5a4a2f1a665df856e7417107e5370
Mat <M.Bavec@student.tudelft.nl>
2023-05-07 17:06:58 +0200

    committed some shit

--- emr-service/routes/emr.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function(req, res, next) {
-  res.redirect(req.headers.referer);
-});
-
-module.exports = router;
