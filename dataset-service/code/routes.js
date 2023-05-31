663e395f31be321a34c1c0e6c7a32ef2bc093ca6
Max Derbenwick <M.A.Derbenwick@student.tudelft.nl>
2023-05-31 18:38:05 +0200

    Finished dataset editor (in theory :))) )

--- dataset-service/code/routes.js
+++ dataset-service/code/routes.js
@@ -44,21 +44,94 @@ router.post('/upload', upload.array('files'), async (req, res) => {
     }
 });
 
-/* POST submit dataset changes. */
-router.post('/submit', async (req, res) => {
+/* POST rename dataset. */
+router.post('/rename', async (req, res) => {
     if(!ensureLoggedIn(req, res)) return;
     const datasetID = req.body['datasetID'];
     const name = req.body['name'];
-    const addLabels = req.body['addLabels'] || [];
-    const editLabels = req.body['editLabels'] || [];
-    const deleteLabels = req.body['deleteLabels'] || [];
-    const editFiles = req.body['editFiles'] || [];
-    const deleteFiles = req.body['deleteFiles'] || [];
 
     try {
-        const labelIDs = await controller.updateDataset(req.session.userID,
-            datasetID, name, addLabels, editLabels, deleteLabels, editFiles, deleteFiles);
-        res.status(labelIDs.length > 0 ? 201 : 200).json({ labelIDs });
+        await controller.renameDataset(req.session.userID, datasetID, name);
+        res.status(204).json();
+    }
+    catch(err) {
+        res.status(400).json({ error: 'Something went wrong' });
+    }
+});
+
+/* POST add label to dataset. */
+router.post('/add-label', async (req, res) => {
+    if(!ensureLoggedIn(req, res)) return;
+    const datasetID = req.body['datasetID'];
+    const string = req.body['string'];
+
+    try {
+        const labelID = await controller.addLabel(req.session.userID, datasetID, string);
+        res.status(201).json({ labelID });
+    }
+    catch(err) {
+        res.status(400).json({ error: 'Something went wrong' });
+    }
+});
+
+/* POST edit label value. */
+router.post('/edit-label', async (req, res) => {
+    if(!ensureLoggedIn(req, res)) return;
+    const datasetID = req.body['datasetID'];
+    const labelID = req.body['labelID'];
+    const string = req.body['string'];
+
+    try {
+        await controller.editLabel(req.session.userID, datasetID, labelID, string);
+        res.status(204).json();
+    }
+    catch(err) {
+        res.status(400).json({ error: 'Something went wrong' });
+    }
+});
+
+/* POST delete label from dataset. */
+router.post('/delete-label', async (req, res) => {
+    if(!ensureLoggedIn(req, res)) return;
+    const datasetID = req.body['datasetID'];
+    const labelID = req.body['labelID'];
+
+    try {
+        await controller.deleteLabel(req.session.userID, datasetID, labelID);
+        res.status(204).json();
+    }
+    catch(err) {
+        res.status(400).json({ error: 'Something went wrong' });
+    }
+});
+
+/* POST submit updated datafile fields. */
+router.post('/update-datafile', async (req, res) => {
+    if(!ensureLoggedIn(req, res)) return;
+    const datasetID = req.body['datasetID'];
+    const datafileID = req.body['datafileID'];
+    const name = req.body['name'];
+    const labelID = req.body['labelID'];
+    const customLabel = req.body['customLabel'];
+
+    try {
+        await controller.updateDatafile(req.session.userID, datasetID, datafileID, name, labelID, customLabel);
+        res.status(204).json();
+    }
+    catch(err) {
+        res.status(400).json({ error: 'Something went wrong' });
+    }
+});
+
+/* POST delete datafile from dataset. */
+router.post('/delete-datafile', async (req, res) => {
+    if(!ensureLoggedIn(req, res)) return;
+    const datasetID = req.body['datasetID'];
+    const datafileID = req.body['datafileID'];
+
+    try {
+        await controller.deleteDatafile(req.session.userID, datasetID, datafileID);
+        res.status(204).json();
     }
     catch(err) {
         res.status(400).json({ error: 'Something went wrong' });
@@ -89,6 +162,20 @@ router.get('/fetch', async (req, res) => {
     }
     catch(err) {
         res.status(400).json({ error: 'Something went wrong' });
+    }
+});
+
+/* GET fetch dataset details. */
+router.get('/fetch-details', async (req, res) => {
+    if(!ensureLoggedIn(req, res)) return;
+    const datasetID = req.query.id;
+
+    try {
+        const dataset = await controller.getDatasetDetails(req.session.userID, datasetID);
+        res.status(200).json(dataset);
+    }
+    catch(err) {
+        res.status(400).json({ error: 'Something went wrong '});
     }
 });
 
