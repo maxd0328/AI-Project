5c6c9e1addcd6e46fa46305c99c27537429b0864
Mat <M.Bavec@student.tudelft.nl>
2023-06-02 16:33:56 +0200

    Added stopping route for EMR

--- emr-service/code/controller.js
+++ emr-service/code/controller.js
@@ -1,27 +1,25 @@
 const AWS = require('../commons/aws').aws;
+const configurations = require('./configuration');
 
-function setUpEMRCluster(name, jarAddress, dataAddress, networkParams) {
-  // Set up the EMR client
-  const emr = new AWS.EMR();
-
+function setUpEMRCluster(emrParameters) {
   // Set up the EMR cluster configuration
   const params = {
-    Name: name,
+    Name: emrParameters.name,
     ReleaseLabel: 'emr-6.5.0',
     Instances: {
       InstanceGroups: [
         {
           InstanceRole: 'MASTER',
           InstanceCount: 1,
-          InstanceType: 'm5.xlarge',
+          InstanceType: emrParameters.emrType,
         },
         {
           InstanceRole: 'CORE',
-          InstanceCount: 2,
-          InstanceType: 'm5.xlarge',
+          InstanceCount: emrParameters.numberOfCores,
+          InstanceType: emrParameters.emrType,
         },
       ],
-      KeepJobFlowAliveWhenNoSteps: true,
+      KeepJobFlowAliveWhenNoSteps: false,
       TerminationProtected: false,
     },
     Applications: [
@@ -35,10 +33,16 @@ function setUpEMRCluster(name, jarAddress, dataAddress, networkParams) {
           Name: 'TrainingJar',
           ActionOnFailure: 'TERMINATE_JOB_FLOW',
           HadoopJarStep: {
-            Jar: jarAddress,
+            Jar: emrParameters.jarAddress,
             Args: [
-              dataAddress,
-              networkParams,
+              emrParameters.dataAddress,
+              emrParameters.configAddress,
+              emrParameters.s3name,
+              emrParameters.region,
+              emrParameters.publicKey,
+              emrParameters.privateKey,
+              emrParameters.newTraining,
+              emrParameters.networkPath,
             ],
           },
         },
@@ -48,12 +52,15 @@ function setUpEMRCluster(name, jarAddress, dataAddress, networkParams) {
   return params;
 }
 
-async function launchEMRCluster(name, jarAddress, dataAddress, networkParams) {
-  const params = await setUpEMRCluster(name, jarAddress, dataAddress, networkParams)
+async function launchEMRCluster(emrParameters) {
+   // Set up the EMR client
+  const emr = new AWS.EMR();
+
+  const params = setUpEMRCluster(emrParameters);
   const data = await emr.runJobFlow(params).promise();
 }
 
-async function addStepToCluster(clusterID, jarAddress, dataAddress, networkParams) {
+async function addStepToCluster(stepName, emrParameters, clusterId) {
       const emr = new AWS.EMR();
 
       const params = {
@@ -63,10 +70,17 @@ async function addStepToCluster(clusterID, jarAddress, dataAddress, networkParam
             Name: stepName,
             ActionOnFailure: 'CONTINUE',
             HadoopJarStep: {
-              Jar: jarAddress,
-              Args:
-              dataAddress,
-              networkParams,
+              Jar: emrParameters.jarAddress,
+              Args: [
+                  emrParameters.dataAddress,
+                  emrParameters.configAddress,
+                  emrParameters.s3name,
+                  emrParameters.region,
+                  emrParameters.publicKey,
+                  emrParameters.privateKey,
+                  emrParameters.newTraining,
+                  emrParameters.networkPath,
+              ]
             },
           },
         ],
@@ -74,4 +88,30 @@ async function addStepToCluster(clusterID, jarAddress, dataAddress, networkParam
 
      await emr.addJobFlowSteps(params).promise();
 
+}
+
+async function getEMRLink(clusterID) {
+    const emr = new AWS.EMR();
+    emr.describeCluster({ ClusterId: clusterId }, function(err, data) {
+      if (err) console.log(err, err.stack); // an error occurred
+      else     console.log(data.Cluster.MasterPublicDnsName); // successful response
+    });
+}
+
+class EMRParams {
+  constructor(userID, name, jarAddress, projectID, s3name, region, publicKey, privateKey, newTraining, networkPath, numberOfCores, emrType) {
+    const [configAddress, dataAddress] = await configurations.createEMRConfiguration(userID, projectID);
+    this.name = name;
+    this.jarAddress = jarAddress;
+    this.dataAddress = dataAddress;
+    this.configAddress = configAddress;
+    this.s3name = s3name;
+    this.region = region;
+    this.publicKey = publicKey;
+    this.privateKey = privateKey;
+    this.newTraining = newTraining;
+    this.networkPath = networkPath;
+    this.numberOfCores = numberOfCores;
+    this.emrType = emrType;
+  }
 }
