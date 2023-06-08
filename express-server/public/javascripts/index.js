2489fdb78962014b5e74beec69bb27d9546e4ffb
Max Derbenwick <M.A.Derbenwick@student.tudelft.nl>
2023-06-09 01:28:23 +0200

    Working on pages

--- express-server/public/javascripts/index.js
+++ express-server/public/javascripts/index.js
@@ -4,3 +4,9 @@ window.addEventListener('scroll', function() {
     let scrolled = window.pageYOffset;
     parallaxBg.style.transform = 'translate3d(0, ' + scrolled * 0.5 + 'px, 0)';
 });
+
+function smoothScroll(target) {
+    document.querySelector(target).scrollIntoView({
+        behavior: 'smooth'
+    });
+}
